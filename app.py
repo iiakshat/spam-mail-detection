0f0bf5876d9611c9f114d0cdf30ecc40bdc96a82
Akshat Sanghvi <92530735+iiakshat@users.noreply.github.com>
2023-02-02 10:41:16 +0530

    Add files

---
+++ app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+from joblib import load
+
+model = load("Classifier.joblib")
+
+def pred(Email):
+    l = model.predict([Email])
+    if l[0]==1:
+        return "Spam ⚠️"
+    else:
+        return "👍"
+
+iface = gr.Interface(fn=pred, inputs="text", outputs="text", allow_flagging="never", description="Enter Your Message Below :")
+iface.launch()
